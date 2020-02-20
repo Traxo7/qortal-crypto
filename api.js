31cc1512dc7175a14480df8b6d54f3d5b1d60a05
irontiga <irontiga@gmail.com>
2020-02-21 00:37:21 +1300

    no message

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+import Base58 from './api/deps/Base58'
+window.Base58 = Base58
+
 export { initApi, store } from './api_deps.js'
 
 export * from './api/deps/deps.js'
--- api/fetch-request.js
+++ api/fetch-request.js
@@ -10,14 +10,12 @@ export async function request(url, options){
 
     await waitForConfig()
     
-    const n = config.user.node
+    const n = config.user.knownNodes[config.user.node]
     const node = n.protocol + '://' + n.domain + ':' + n.port
-    console.log(body)
     return fetch(node + url, {
         method,
         body // If it's undefined that's fine right?
     }).then(async response => {
-        console.log(response)
         try {
             const json = await response.clone().json()
             return json

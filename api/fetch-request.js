c8bbe57c0e458b4d1b3be45c72125f1b60c4e324
LOTW <lotw7270@protonmail.com>
2021-01-07 10:49:01 +0100

    Refactored fetch request

--- api/fetch-request.js
+++ api/fetch-request.js
@@ -1,28 +1,30 @@
 import { watchConfig, waitForConfig } from '../config.js'
 
 let config = {}
-watchConfig(c => { config = c })
+watchConfig((c) => {
+	config = c
+})
 
 export async function request(url, options) {
-    options = options || {}
-    const body = options.body
-    const method = options.method || 'GET'
-    const headers = options.headers || {}
+	options = options || {}
+	const body = options.body
+	const method = options.method || 'GET'
+	const headers = options.headers || {}
 
-    await waitForConfig()
+	await waitForConfig()
 
-    const n = config.nodeConfig.knownNodes[config.nodeConfig.node]
-    const node = n.protocol + '://' + n.domain + ':' + n.port
-    return fetch(node + url, {
-        method,
-        headers,
-        body // If it's undefined that's fine right?
-    }).then(async response => {
-        try {
-            const json = await response.clone().json()
-            return json
-        } catch (e) {
-            return await response.text()
-        }
-    })
+	const n = config.nodeConfig.knownNodes[config.nodeConfig.node]
+	const node = n.protocol + '://' + n.domain + ':' + n.port
+	return fetch(node + url, {
+		method,
+		headers,
+		body,
+	}).then(async (response) => {
+		try {
+			const json = await response.clone().json()
+			return json
+		} catch (e) {
+			return await response.text()
+		}
+	})
 }
